2f4f1e0095dc0939fa2b86c28ac35fc9132902f9
harshbawari <harshbawari@gmail.com>
2020-10-06 21:39:14 +0530

    Removed Gestures Part 2 becuase of Errors

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -45,7 +45,7 @@ function RenderDish(props) {
         toggleShowModal(false);
     }
 
-    handleViewRef = ref => this.view = ref;
+
 
     const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
         if (dx < -200)
@@ -57,10 +57,6 @@ function RenderDish(props) {
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: (e, gestureState) => {
             return true;
-        },
-        onPanResponderGrant: () => {
-            this.view.rubberBand(1000)
-                .then(endState => console.log(endState.finished ? 'finished' : 'cancelled'))
         },
         onPanResponderEnd: (e, gestureState) => {
             console.log("pan responder end", gestureState);
@@ -89,9 +85,9 @@ function RenderDish(props) {
     if (dish != null) {
         return (
             <Animatable.View
-                animation="fadeInDown" delay={1000}
+                animation="fadeInDown"
+                delay={1000}
                 duration={2000}
-                ref={this.handleViewRef}
                 {...panResponder.panHandlers}
             >
                 <Card
